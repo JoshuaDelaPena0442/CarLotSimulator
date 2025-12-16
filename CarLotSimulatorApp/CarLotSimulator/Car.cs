c8a94431707855d0bcdc0007ed4c70b4196d7290
Joshua Dela Pena <shiroganebigboi@gmail.com>
2025-12-16 15:12:47 -0600

    Completed exercise

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,38 @@
+using System;
+
+namespace CarLotSimulator;
+
+public class Car
+{
+    public string Make { get; set; }
+    public string Model { get; set; }
+    public int Year { get; set; }
+    public string EngineNoise { get; set; }
+    public string HonkNoise { get; set; }
+    public bool IsDriveable { get; set; }
+
+    public void MakeEngineNoise(string engineNoise)
+    {
+        EngineNoise = engineNoise;
+        Console.WriteLine($"{Make} {Model}'s, make this engine sound {EngineNoise}");
+    }
+
+    public void MakeHonkNoise(string honkNoise)
+    {
+        HonkNoise = honkNoise;
+        Console.WriteLine($"{Make} {Model}'s, honk like this {HonkNoise}");
+    }
+
+    public Car()
+    {
+        
+    }
+
+    public Car(string make, string model, int year, bool isDriveable)
+    {
+        Make = make;
+        Model = model;
+        Year = year;
+        IsDriveable = isDriveable;
+    }
+}
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -6,9 +6,10 @@ namespace CarLotSimulator
     {
         static void Main(string[] args)
         {
+            var carLotOne = new CarLot();
+            
             //TODO
-
-            //Create a seperate class file called Car
+            //Create a separate class file called Car
             //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
             //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
             //The methods should take one string parameter: the respective noise property
@@ -17,6 +18,43 @@ namespace CarLotSimulator
             //Now that the Car class is created we can instanciate 3 new cars
             //Set the properties for each of the cars
             //Call each of the methods for each car
+
+            var carOne = new Car();
+            carOne.Make = "Honda";
+            carOne.Model = "Civic";
+            carOne.Year = 1972;
+            carOne.IsDriveable = true;
+            
+            carOne.MakeEngineNoise("Vrooom");
+            carOne.MakeHonkNoise("Beep");
+
+            
+
+            var carTwo = new Car()
+            {
+                Make = "Jeep",
+                Model = "Wrangler",
+                Year = 1986,
+                IsDriveable = true,
+            };
+            
+            carTwo.MakeEngineNoise("Vrooom Vrooom");
+            carTwo.MakeHonkNoise("Beep Beep");
+            
+            
+
+            var carThree = new Car("Nissan", "Altima", 1982, true);
+            carThree.MakeEngineNoise("Vrooom Vrooom Vroooom");
+            carThree.MakeHonkNoise("Beeep Beeep Beeep");
+            
+            
+            carLotOne.ParkingLot.Add(carOne);
+            carLotOne.ParkingLot.Add(carTwo);
+            carLotOne.ParkingLot.Add(carThree);
+            
+            carLotOne.CheckCars();
+
+
 
             //*************BONUS*************//
 
